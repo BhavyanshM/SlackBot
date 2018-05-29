694b26ce7d0d4e7525f0aedd7efb303484f7acdb
Bhavyansh Mishra <bmishra@ihmc.us>
2018-05-29 16:46:55 -0500

    Add the integration with Bamboo for the Slack bot and parse the JSON

--- src/main/kotlin/us/ihmc/HelloWorld.kt
+++ src/main/kotlin/us/ihmc/HelloWorld.kt
@@ -1,14 +1,53 @@
 package us.ihmc
 
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.omg.PortableInterceptor.USER_EXCEPTION
+import org.json.JSONObject as JSONObject
+import javax.swing.text.html.parser.Parser
+
+
 
 fun main(args: Array<String>) {
-	val session = SlackSessionFactory.createWebSocketSlackSession("AUTH_TOKEN_FOR_SLACK_BOT_USER");
+
+    val PLAN_NUMBER = 23
+    val BAMBOO_USERNAMe = "bmishra"
+    val BAMBOO_PASSWORD = "mishra@ihmc"
+    val SLACK_ACCESS_TOKEN = "xoxb-157670531812-371945587298-Y60nAVtKQFL0zYRRTZbgIXjW"
+
+	val session = SlackSessionFactory.createWebSocketSlackSession(SLACK_ACCESS_TOKEN);
 	session.connect();
 	session.addMessagePostedListener({ posted, session ->
-		if (posted.sender.userName == "Bhavyansh Mishra")
-			session.sendMessage(posted.channel, "This is infinite automated Slack bot.", null);
+//		if (posted.sender.userName == "bhavyansh")
+//		{}
+		if(posted.messageContent.contains("+release status")){
+            val client = OkHttpClient();
+            val request = Request.Builder()
+                    .url("https://bamboo.ihmc.us/rest/api/latest/result.json?os_authType=basic")
+                    .header("Authorization", Credentials.basic(BAMBOO_USERNAMe, BAMBOO_PASSWORD))
+                    .build()
+            val response = client.newCall(request).execute()
+            println(response)
+            val data = response.body()!!.string()
+            val json = JSONObject(data)
+            val planShortName = json.getJSONObject("results")
+                    .getJSONArray("result")
+                    .getJSONObject(PLAN_NUMBER)
+                    .getJSONObject("plan")
+                    .getString("shortName")
+            val result = json.getJSONObject("results")
+                    .getJSONArray("result")
+                    .getJSONObject(PLAN_NUMBER)
+                    .getString("state").toString()
+            val output = "${planShortName}\nBuild Status: ${result}"
+			session.sendMessage(posted.channel, output, null);
+		}
 	});
+
+
+
 }
 
 
